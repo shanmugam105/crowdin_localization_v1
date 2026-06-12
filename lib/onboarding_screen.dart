a042934d42c37639de947c3bec47899d89f173d2
Shanmugarajeshwaran R <shanmugarajeshwaran@sparkouttech.com>
2026-06-12 19:06:09 +0530

    Remove commented-out debug line in _OnboardingScreenState class

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -33,7 +33,6 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     super.dispose();
   }
 
-  //HEllo
   @override
   Widget build(BuildContext context) {
     bool showFinalImage = (_page > 3 && _page < 4);
