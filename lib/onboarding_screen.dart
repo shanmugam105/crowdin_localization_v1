234050d749e4031b17bed054356b9203b1aa5e3a
Shanmugarajeshwaran R <shanmugarajeshwaran@sparkouttech.com>
2026-06-13 11:40:24 +0530

    Add a placeholder comment in _OnboardingScreenState class

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -33,6 +33,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     super.dispose();
   }
 
+  //sfsdfshduk
   @override
   Widget build(BuildContext context) {
     bool showFinalImage = (_page > 3 && _page < 4);
